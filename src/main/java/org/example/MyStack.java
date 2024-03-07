27bbfbf3519582c5f6562b8fb4f6477906b5ee01
krisha7desai <krishad2001@gmail.com>
2024-03-06 20:02:49 -0500

    initialized as "this"

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,6 +3,13 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 public class MyStack<E> extends ArrayList<E> {
+    private ArrayList<E> delegate;
+
+
+
+    public MyStack() {
+        delegate = this;
+    }
     public void push(E e) {
         add(e);
     }
